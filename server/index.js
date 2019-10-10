88e6b6342589b328f63668474c6fd55cc56297aa
Elizabeth Gramzow <ggram001@plattsburgh.edu>
2019-10-10 11:44:00 -0400

    using env port variable

--- server/index.js
+++ server/index.js
@@ -5,9 +5,11 @@ const gameController = require('./controllers/Game');
 
 
 const app = express();
-const port = 3000;
+const port = 3000; //process.env.PORT || 3000
 
 app
+    .get('/port', (req, res) => res.send("Using port: " + port))
+
     .use('/static', express.static(path.join(__dirname, '../NoFramework')))
     
     .use('/users', userController)
